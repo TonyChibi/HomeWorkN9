0f5baaec5440d856b65d25f9a4f4a2daf2940a2e
GlazHorsez <to5gogol@gmail.com>
2023-06-19 13:51:00 +0300

    added task 64 and gitignore files

---
+++ task64/Program.cs
@@ -0,0 +1,9 @@
+﻿string BackCount(int n){
+    if (n==1)return $"{n}";
+    else return $"{n}, {BackCount(n-1)}";
+}
+
+
+Console.Write("Insert some integer: ");
+int n=int.Parse(Console.ReadLine()!);
+Console.WriteLine(BackCount(n));
