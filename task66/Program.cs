c59e25337b8deb29b8f079344af99fefd4b914a0
GlazHorsez <to5gogol@gmail.com>
2023-06-19 20:37:54 +0300

    add task N 66

---
+++ task66/Program.cs
@@ -0,0 +1,10 @@
+﻿int Sum(int n,int m){
+    if(m==n)return n;
+    else return Sum(n,m-1)+m;
+}
+
+Console.Clear();
+Console.Write("Insert two integers: ");
+int [] nums=Console.ReadLine()!.Split().Select(x=>int.Parse(x)).ToArray();
+int sum=Sum(nums[0],nums[1]);
+Console.WriteLine(sum);
