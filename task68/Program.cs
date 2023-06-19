bd2635b74a18b33891423d1703added62f213be8
GlazHorsez <to5gogol@gmail.com>
2023-06-19 23:47:56 +0300

    added task N66

---
+++ task68/Program.cs
@@ -0,0 +1,13 @@
+﻿int Ackermann(int m=2, int n=3){
+    if(m==0)return n+1;
+    if (n==0)return Ackermann(m-1,1);
+    return Ackermann(m-1,Ackermann(m,n-1));
+   
+}
+
+
+
+Console.Clear();
+Console.WriteLine("insert two integers: ");
+int [] nums=Console.ReadLine()!.Split().Select(x=>int.Parse(x)).ToArray();
+Console.WriteLine(Ackermann(nums[0],nums[1]));
